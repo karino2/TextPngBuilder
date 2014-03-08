09b81bbf93ce816495a3a291448304273c884a0a
karino <hogeika2@gmail.com>
2014-03-08 23:08:48 +0900

    Support vertical.

--- app/src/main/java/com/livejournal/karino2/textpngbuilder/TextPngBuilderActivity.java
+++ app/src/main/java/com/livejournal/karino2/textpngbuilder/TextPngBuilderActivity.java
@@ -5,16 +5,24 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Picture;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.webkit.WebView;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import java.io.File;
@@ -22,7 +30,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class TextPngBuilderActivity extends ActionBarActivity {
 
@@ -33,24 +43,118 @@ public class TextPngBuilderActivity extends ActionBarActivity {
 
     }
 
-    private void handleDone() {
-        TextImage textImage = new TextImage();
-        textImage.resize(600, 400);
-        EditText et = (EditText)findViewById(R.id.editText);
-        String text = et.getText().toString();
+    private List<String> stringToStringList(String text) {
+        ArrayList<String> list = new ArrayList<String>();
         int lastEol = 0;
         while(text.length() > lastEol) {
             int eol = text.indexOf('\n', lastEol);
             if(eol == -1) {
-                textImage.addLine(text.substring(lastEol));
-                break;
+                list.add(text.substring(lastEol));
+                return list;
             }
-            textImage.addLine(text.substring(lastEol, eol));
+            list.add(text.substring(lastEol, eol));
             lastEol = eol+1;
+        }
+        return list;
+    }
+
+    private void handleDone() {
+        EditText et = (EditText)findViewById(R.id.editText);
+        String text = et.getText().toString();
+        List<String> strings = stringToStringList(text);
+
+        if(isChecked(R.id.checkVertical)) {
+            handleDoneWithVerticalTextList(strings);
+        }else {
+            Bitmap bitmap = textListToBitmap(strings);
+            handleDoneWithBitmap(bitmap);
+        }
+    }
+
+
+    boolean isTest() {
+        return isChecked(R.id.checkTest);
+    }
+
+    private boolean isChecked(int resId) {
+        CheckBox box = (CheckBox)findViewById(resId);
+        return box.isChecked();
+    }
+
+    PopupWindow popupWindow;
+    View popupView;
+    private void handleDoneWithVerticalTextList(List<String> strings) {
+        if(popupView == null) {
+            LayoutInflater inflater = getLayoutInflater();
+            popupView = inflater.inflate(R.layout.popup_webview, null);
+            WebView webView = (WebView)popupView.findViewById(R.id.webView);
+
+            webView.setPictureListener(new WebView.PictureListener() {
+                @Override
+                public void onNewPicture(WebView view, Picture picture) {
+                    if(!isTest()) {
+                        Picture pictureObj = view.capturePicture();
+
+                        Bitmap  bitmap = Bitmap.createBitmap(
+                                pictureObj.getWidth(),
+                                pictureObj.getHeight(),
+                                Bitmap.Config.ARGB_8888);
+
+                        Canvas canvas = new Canvas(bitmap);
+                        pictureObj.draw(canvas);
+                        whiteToTransparent(bitmap);
+
+                        handleDoneWithBitmap(bitmap);
+                    }
+                }
+            });
+
+            View editText = findViewById(R.id.editText);
+            popupWindow = new PopupWindow(popupView,editText.getMeasuredWidth(), editText.getMeasuredHeight()*3/4, false);
+//            popupWindow = new PopupWindow(popupView, 600, 400, false);
+            popupWindow.showAtLocation(findViewById(R.id.editText), Gravity.BOTTOM, 0, 0);
+        }
+
+        WebView webView = (WebView)popupView.findViewById(R.id.webView);
+        StringBuilder builder = new StringBuilder();
+        builder.append("<html><body style=\"-webkit-writing-mode: vertical-rl;font-size: x-large;\">");
+        for(String line : strings) {
+            builder.append(escapeHtml(line));
+            builder.append("<br>");
+        }
+        builder.append("</body></html>");
+
+
+        webView.loadData(builder.toString(), "text/html; charset=UTF-8", null);
+
+
+}
+
+    private void whiteToTransparent(Bitmap bitmap) {
+        int[] buf = new int[bitmap.getWidth()*bitmap.getHeight()];
+        bitmap.getPixels(buf, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        for(int i = 0; i < bitmap.getWidth()*bitmap.getHeight(); i++) {
+            if(buf[i] == Color.WHITE) {
+                buf[i] = Color.TRANSPARENT;
+            }
+        }
+        bitmap.setPixels(buf, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+    }
+
+    private String escapeHtml(String line) {
+        return line.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+    }
+
+
+    private Bitmap textListToBitmap(List<String> strings) {
+        TextImage textImage = new TextImage();
+        textImage.resize(600, 400);
+        for(String str : strings) {
+            textImage.addLine(str);
         }
         textImage.autoCharSize();
         textImage.rasterlize();
-        handleDoneWithBitmap(textImage.bitmap());
+        return textImage.bitmap();
     }
 
 
